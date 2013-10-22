7a3255c4d2dea3a9d598565b446bf4b1edab49cc
Dublin R <DublinRUserGroup@gmail.com>
2013-10-22 23:52:56 +0100

    Create LearnBayes.R

---
+++ LearnBayes.R
@@ -0,0 +1,11 @@
+# Package ‘LearnBayes’
+# August 29, 2013
+
+# LearnBayes contains a collection of functions helpful in learning the basic tenets of Bayesian statistical inference.
+# It contains functions for summarizing basic one and two parameter posterior distributions and predictive distributions.
+# It contains MCMC algorithms for summarizing posterior distributions deﬁned by the user. 
+# It also contains functions for regression models, hierarchical models, Bayesian tests, and 
+# illustrations of Gibbs sampling
+
+library("LearnBayes")
+findBeta(quantile1,quantile2,quantile3)
